fdd103e68a40a625375282a3f8d7c0a6a84c7ae6
can019 <jys01012@gmail.com>
2024-07-05 06:34:11 +0900

    fea: Test시 성능 측정을 위한 TestExecutionListener 작성   - AbstractTestTimeExecutionListener     - Total test 시간 측정   - TestTimeExecutionListener     - Test시 각 메소드별 시간 측정 진행     - StopWatchUtil을 통해 csv로 export

---
+++ src/integration/java/com/github/can019/performance/test/util/TestTimeExecutionListener.java
@@ -0,0 +1,46 @@
+package com.github.can019.performance.test.util;
+
+import org.springframework.test.context.TestContext;
+import org.springframework.util.StopWatch;
+
+import java.util.UUID;
+
+public class TestTimeExecutionListener extends AbstractTestTimeExecutionListener {
+
+    private StopWatch stopWatch;
+
+    @Override
+    public void beforeTestClass(TestContext testContext) throws Exception {
+        super.beforeTestClass(testContext);
+        stopWatch = new StopWatch(testContext.getTestClass().getSimpleName());
+    }
+
+    @Override
+    public void beforeTestMethod(TestContext testContext) throws Exception {
+        super.beforeTestMethod(testContext);
+        stopWatch.start(testContext.getTestMethod().getName());
+    }
+
+    @Override
+    public void afterTestMethod(TestContext testContext) throws Exception {
+        if (stopWatch.isRunning()) {
+            stopWatch.stop();
+        }
+        super.afterTestMethod(testContext);
+    }
+
+    @Override
+    public void afterTestClass(TestContext testContext) throws Exception {
+        super.afterTestClass(testContext);
+        System.out.println("Test '" + testContext.getTestClass().getSimpleName() + "' finished after " + stopWatch.getTotalTimeSeconds() + " seconds.");
+    }
+
+    private String exportCsvPathResolver(TestContext testContext){
+        String packageName = testContext.getTestClass().getPackageName();
+        String className = testContext.getTestClass().getSimpleName();
+        String csvFilePath = "./test/reports/" + packageName
+                + "/" + className + "/"+ UUID.randomUUID()+".csv";
+
+        return csvFilePath;
+    }
+}
