f62c58c5115dd382a7d8621b9629b0d929b04f5c
can019 <jys01012@gmail.com>
2024-07-05 05:03:46 +0900

    chore:init

---
+++ src/main/java/com/github/can019/performance/MysqlPrimaryKeyTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.can019.performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlPrimaryKeyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlPrimaryKeyTestApplication.class, args);
+	}
+
+}
