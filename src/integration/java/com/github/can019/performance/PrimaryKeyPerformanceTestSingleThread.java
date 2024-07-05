748c2bd7182fde76d43506f38c4bd928d16980b6
can019 <jys01012@gmail.com>
2024-07-05 17:31:02 +0900

    chore: 기존 PrimaryKeyTest class 이름에 SingleThread 추가

--- src/integration/java/com/github/can019/performance/PrimaryKeyPerformanceTestSingleThread.java
+++ src/integration/java/com/github/can019/performance/PrimaryKeyPerformanceTestSingleThread.java
@@ -32,7 +32,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @TestExecutionListeners(value = {TestTimeExecutionListener.class}, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Commit
-public class PrimaryKeyPerformanceTest {
+public class PrimaryKeyPerformanceTestSingleThread {
 
     @PersistenceContext
     private EntityManager em;
