754e8a11bc6bbe1e732547a0b107c8b4d68acd53
Yousung Jung <jys01012@gmail.com>
2024-07-05 17:03:02 +0900

    feat: integration, unit test source set 추가 (#1) (#2)

--- src/test/java/com/github/can019/performance/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.github.can019.performance;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
