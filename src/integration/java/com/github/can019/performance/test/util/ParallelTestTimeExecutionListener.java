06bfae6f6a2d0fbced2108039b5404c199114e93
can019 <jys01012@gmail.com>
2024-07-05 17:26:25 +0900

    feat: 병렬 test 시간 측정을 위한 TestExecutionListner   - 직접적으로 시간 측정을 하지 않음   - ThreadLocal에서 StopWatch를 꺼내 쓸 수 있도록 함     - ThreadLocal은 public

---
+++ src/integration/java/com/github/can019/performance/test/util/ParallelTestTimeExecutionListener.java
@@ -0,0 +1,38 @@
+package com.github.can019.performance.test.util;
+
+import com.github.can019.performance.test.util.stopwatch.StopWatchUtil;
+import org.springframework.test.context.TestContext;
+import org.springframework.util.StopWatch;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ParallelTestTimeExecutionListener extends AbstractTestTimeExecutionListener {
+
+    public static final ThreadLocal<StopWatch> threadLocalStopWatch =
+            ThreadLocal.withInitial(StopWatch::new);
+
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+    private final Date startedTime = new Date();
+
+    @Override
+    public void afterTestMethod(TestContext testContext) throws Exception {
+        super.afterTestMethod(testContext);
+        StopWatch stopWatch = ParallelTestTimeExecutionListener.threadLocalStopWatch.get();
+        StopWatchUtil.exportCsv(stopWatch, exportCsvPathResolver(testContext));
+        threadLocalStopWatch.remove();
+    }
+
+    private String exportCsvPathResolver(TestContext testContext){
+        String packageName = testContext.getTestClass().getPackageName();
+        String className = testContext.getTestClass().getSimpleName();
+        String csvFilePath = String.join("/",
+                "./test/reports",
+                packageName,
+                className,
+                dateFormat.format(startedTime),
+                testContext.getTestMethod().getName()) + ".csv";
+
+        return csvFilePath;
+    }
+}
